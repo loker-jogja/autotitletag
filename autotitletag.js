c759c51e9e937a74bcaf9a4119209ed620085bca
Loker Jogja <125717321+loker-jogja@users.noreply.github.com>
2023-03-18 11:27:04 +0700

    Update autotitletag.js

--- autotitletag.js
+++ autotitletag.js
@@ -6,6 +6,6 @@ for (let i = 0; i < links.length; i++) {
   // Periksa apakah tag <a> memiliki atribut title
   if (!links[i].hasAttribute("title")) {
     // Jika tidak ada, tambahkan atribut title dengan nilai isi dari tag <a>
-    links[i].setAttribute("title", links[i].innerHTML);
+    links[i].setAttribute("title", links[i].textContent.trim());
   }
 }
